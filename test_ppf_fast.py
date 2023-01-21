bc461c1cb81ec7ce04113c51d4dc28182bd662d4
max <max@thousandyardstare.de>
2023-01-21 12:18:33 +0100

    Improve testing of cpp equivalent

--- test_ppf_fast.py
+++ test_ppf_fast.py
@@ -7,35 +7,42 @@ import ppf
 
 
 def test_vector_angle():
-    vecA = np.random.uniform(size=3)
-    vecB = np.random.uniform(size=3)
+    np.random.seed(123)
+    
+    for i in range(50):
+        vecA = np.random.uniform(size=3)
+        vecB = np.random.uniform(size=3)
 
-    vecA /= np.linalg.norm(vecA)
-    vecB /= np.linalg.norm(vecB)
-    print(vecA)
-    print(vecB)
+        vecA /= np.linalg.norm(vecA)
+        vecB /= np.linalg.norm(vecB)
+        print(vecA)
+        print(vecB)
 
-    anglepy = trimesh.geometry.vector_angle([(vecA, vecB)])[0]
-    print(anglepy)
+        anglepy = trimesh.geometry.vector_angle([(vecA, vecB)])[0]
+        print(anglepy)
 
-    anglecpp = ppf_fast.vector_angle(vecA, vecB)
-    print(anglecpp)
+        anglecpp = ppf_fast.vector_angle(vecA, vecB)
+        print(anglecpp)
 
-    assert math.isclose(anglepy, anglecpp, abs_tol=1e-6)
+        assert math.isclose(anglepy, anglecpp, abs_tol=1e-6)
 
 def test_compute_feature():
     step_rad = 0.1
     step_dist = 0.1
 
-    vertA = np.array([0,0,0])
-    vertB = np.array([1,1,1])
-    normA = np.array([1,0,0])
-    normB = np.array([0,1,0])
+    np.random.seed(123)
+    for i in range(50):
+        vertA = np.random.uniform(-10,10, size=3)
+        vertB = np.random.uniform(-10,10, size=3)
+        normA = np.random.uniform(size=3)
+        normB = np.random.uniform(size=3)
+        normA /= np.linalg.norm(normA)
+        normB /= np.linalg.norm(normB)
 
-    Fpy = ppf.compute_feature(vertA, vertB, normA, normB, step_rad, step_dist)
-    print("Fpy", Fpy)
+        Fpy = ppf.compute_feature(vertA, vertB, normA, normB, step_rad, step_dist)
+        print("Fpy", Fpy)
 
-    Fcpp = ppf_fast.compute_feature(vertA, vertB, normA, normB, step_rad, step_dist)
-    print("Fcpp", Fcpp)
+        Fcpp = ppf_fast.compute_feature(vertA, vertB, normA, normB, step_rad, step_dist)
+        print("Fcpp", Fcpp)
 
-    assert np.allclose(Fcpp, Fpy)
+        assert np.allclose(Fcpp, Fpy)
