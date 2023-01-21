4a354d13b9747bc0847895d5a327065679ccc086
max <max@thousandyardstare.de>
2023-01-21 14:38:29 +0100

    Add alignVectors and vectorAngleSignedX

--- ppf.cc
+++ ppf.cc
@@ -47,12 +47,34 @@ Feature computeFeature(const Vec3 &vecA, const Vec3 &vecB, const Vec3 &normA,
   return {F1, F2, F3, F4};
 }
 
+double vectorAngleSignedX(const Vec3 &vecA, const Vec3 &vecB) {
+  assert(std::abs(vecA.norm() - 1) <= 1e-3);
+  assert(std::abs(vecB.norm() - 1) <= 1e-3);
+  return std::atan2(vecA.cross(vecB).dot(Vec3(1, 0, 0)), vecA.dot(vecB));
+}
+
+Eigen::Matrix3d alignVectors(const Vec3 &a, const Vec3 &b) {
+  const Vec3 v = a.cross(b);
+  const double c = a.dot(b);
+  const double h = 1 / (1 + c);
+
+  const double v1 = v(0);
+  const double v2 = v(1);
+  const double v3 = v(2);
+
+  Eigen::Matrix3d Vmat;
+  Vmat << 0, -v3, v2, v3, 0, -v1, -v2, v1, 0;
+
+  return Eigen::Matrix3d::Identity() + Vmat + Vmat * Vmat * h;
+}
+
 Pair2Feature computePPF(const Vecs3 &verts, const Vecs3 &normals,
-                        double step_rad, double step_dist) {
+                        double step_rad, double step_dist,
+                        bool alphas = false) {
   Pair2Feature ref2feature;
 
-  std::cout << "verts" << verts << "\n";
-  std::cout << "normals" << normals << "\n";
+  // std::cout << "verts" << verts << "\n";
+  // std::cout << "normals" << normals << "\n";
 
   const int num_verts = verts.rows();
 
@@ -74,6 +96,24 @@ Pair2Feature computePPF(const Vecs3 &verts, const Vecs3 &normals,
         continue;
 
       ref2feature[{i, j}] = F;
+
+      if (alphas) {
+        const auto &m_r = vertA;
+        const auto &m_i = vertB;
+        const auto &m_normal = normA;
+
+        Eigen::Matrix3d R_model2glob = Eigen::Matrix3d::Identity();
+        // R_model2glob.block<3, 3>(0, 0) = alignVectors(m_normal, Vec3(1, 0,
+        // 0));
+      }
+      // R_model2glob = np.eye(4)
+      // R_model2glob[:3, :3] = align_vectors(m_normal, [1, 0, 0])
+      // T_model2glob = R_model2glob @ tf.translation_matrix(-m_r)
+
+      // m_ig = (T_model2glob @ homog(m_i))[:3]
+      // m_ig /= np.linalg.norm(m_ig)
+      // alpha_m = vector_angle_signed_x(m_ig, [0, 0, -1])
+      // model_alphas[(ivertA, ivertB)] = alpha_m
     }
   }
 
@@ -89,7 +129,7 @@ const auto toMatX3 = [](const nbMatX3 &mat) {
   return Eigen::Map<const Vecs3>(mat.data(), mat.shape(0), mat.shape(1));
 };
 
-const auto toVec3f = [](const nbVec3 &vec) {
+const auto toVec3 = [](const nbVec3 &vec) {
   return Eigen::Map<const Vec3>(vec.data(), 1, 3);
 };
 
@@ -100,17 +140,35 @@ NB_MODULE(ppf_fast, m) {
   });
 
   m.def("vector_angle", [](const nbVec3 &vecA, const nbVec3 &vecB) {
-    return vectorAngle(toVec3f(vecA), toVec3f(vecB));
+    return vectorAngle(toVec3(vecA), toVec3(vecB));
+  });
+
+  m.def("vector_angle_signed_x", [](const nbVec3 &vecA, const nbVec3 &vecB) {
+    return vectorAngleSignedX(toVec3(vecA), toVec3(vecB));
+  });
+
+  m.def("align_vectors", [&](const nbVec3 &vecA, const nbVec3 &vecB) {
+    size_t shape[2] = {3, 3};
+    auto R = alignVectors(toVec3(vecA), toVec3(vecB));
+
+    // Ugly construction to preserve lifetime while python uses it
+    // not sure how to make this nicer
+    double *data = new double[9];
+    std::memcpy(data, R.data(), sizeof(data[0]) * 9);
+    nb::capsule owner(data, [](void *p) noexcept { delete[](double *) p; });
+
+    // Since eigen is column major, we need to jump three entries to get one
+    // place to the right. And one to jump down.
+    int64_t strides[2] = {1, 3};
+
+    return nb::tensor<nb::numpy, double, nb::shape<3, 3>, nb::f_contig>(
+        data, 2, shape, owner, strides);
   });
 
   m.def("compute_feature",
         [](const nbVec3 &vecA, const nbVec3 &vecB, const nbVec3 &normA,
            const nbVec3 &normB, double step_rad, double step_dist) {
-          return computeFeature(toVec3f(vecA), toVec3f(vecB), toVec3f(normA),
-                                toVec3f(normB), step_rad, step_dist);
+          return computeFeature(toVec3(vecA), toVec3(vecB), toVec3(normA),
+                                toVec3(normB), step_rad, step_dist);
         });
 }
-
-// correct order
-// Eigen::Map<const Vecs3, Eigen::Unaligned, Eigen::Stride<1, 3>>(verts.data(),
-// verts.shape(0), verts.shape(1)),
--- test_ppf_fast.py
+++ test_ppf_fast.py
@@ -6,10 +6,28 @@ import ppf_fast
 import ppf
 
 
+def test_align_vectors():
+    np.random.seed(123)
+
+    for _ in range(50):
+        vecA = np.random.uniform(size=3)
+        vecB = np.random.uniform(size=3)
+
+        vecA /= np.linalg.norm(vecA)
+        vecB /= np.linalg.norm(vecB)
+
+        R_py = ppf.align_vectors(vecA, vecB)
+        R_cc = ppf_fast.align_vectors(vecA, vecB)
+        print("ref\n",R_py)
+        print("cc\n", R_cc)
+
+        assert np.allclose(R_cc, R_py)
+
+
 def test_vector_angle():
     np.random.seed(123)
-    
-    for i in range(50):
+
+    for _ in range(50):
         vecA = np.random.uniform(size=3)
         vecB = np.random.uniform(size=3)
 
@@ -19,12 +37,13 @@ def test_vector_angle():
         print(vecB)
 
         anglepy = trimesh.geometry.vector_angle([(vecA, vecB)])[0]
-        print(anglepy)
-
         anglecpp = ppf_fast.vector_angle(vecA, vecB)
-        print(anglecpp)
-
         assert math.isclose(anglepy, anglecpp, abs_tol=1e-6)
+
+        anglepy_signed = ppf.vector_angle_signed_x(vecA, vecB)
+        anglecpp_signed = ppf_fast.vector_angle_signed_x(vecA, vecB)
+        assert math.isclose(anglepy_signed, anglecpp_signed)
+
 
 def test_compute_feature():
     step_rad = 0.1
@@ -32,8 +51,8 @@ def test_compute_feature():
 
     np.random.seed(123)
     for i in range(50):
-        vertA = np.random.uniform(-10,10, size=3)
-        vertB = np.random.uniform(-10,10, size=3)
+        vertA = np.random.uniform(-10, 10, size=3)
+        vertB = np.random.uniform(-10, 10, size=3)
         normA = np.random.uniform(size=3)
         normB = np.random.uniform(size=3)
         normA /= np.linalg.norm(normA)
@@ -47,14 +66,13 @@ def test_compute_feature():
 
         assert np.allclose(Fcpp, Fpy)
 
+
 def test_compute_ppf():
     mesh = trimesh.load("./example_models/model_small.ply")
     step_rad = 0.1
     step_dist = 0.1
 
-    _, pairs_model_py, _ = ppf.compute_ppf(
-        mesh, step_rad, step_dist, alphas=False
-    )
+    _, pairs_model_py, _ = ppf.compute_ppf(mesh, step_rad, step_dist, alphas=False)
 
     # Current bug in nanobind: Doesn't recognize write protected arrays
     F_verts = np.asfortranarray(mesh.vertices)
@@ -62,14 +80,12 @@ def test_compute_ppf():
     F_norms = np.asfortranarray(mesh.vertex_normals)
     F_norms.setflags(write=True)
 
-    pairs_model_cpp = ppf_fast.compute_ppf(
-        F_verts, F_norms, step_rad, step_dist
-    )
+    pairs_model_cpp = ppf_fast.compute_ppf(F_verts, F_norms, step_rad, step_dist)
 
     for key in pairs_model_cpp:
         pypair = pairs_model_py[key[0]][key[1]]
         if pypair is None:
             continue
-        
+
         # Allow result to be off by one bin in any of the features
         assert np.sum(np.subtract(pypair, pairs_model_cpp[key])) <= 1
